53ed27c112fed019795b525428660acff975f5b1
Dan Conley <dconley@protonmail.com>
2020-02-09 16:43:27 -0500

    ask for lucky egg on friendship

--- v2/bot/pvp.go
+++ v2/bot/pvp.go
@@ -34,6 +34,15 @@ func init() {
 	registerCommand("pvp", p.Handle, "PVP friend tracking/battle announcing. `pvp help` for more details")
 }
 
+type Answer int
+
+const (
+	AnswerUnknown Answer = iota
+	AnswerYes
+	AnswerNo
+	AnswerCancel
+)
+
 type PVP struct {
 	registering map[string]pvp.Player
 	battling    map[string]map[string]*time.Timer
@@ -118,7 +127,7 @@ func (p *PVP) StopBattling(m *discordgo.MessageCreate) string {
 func (p *PVP) GetPlayers(server string) []pvp.Player {
 	var players []pvp.Player
 
-	req, err := http.NewRequest(http.MethodGet, pvpURL+"/list?server="+server, nil)
+	req, err := http.NewRequest(http.MethodGet, pvpURL+"/player/list?server="+server, nil)
 	if err != nil {
 		log.Printf("error creating player list request: %s", err.Error())
 		return players
@@ -210,30 +219,55 @@ func (p *PVP) SaveFriendCode(pieces []string, m *discordgo.MessageCreate, s *dis
 	}
 	player.FriendCode = strings.Join(pieces, "")
 	p.registering[m.Author.ID] = player
+	expectPM(m.ChannelID, p.EggForUltra)
+	return "Do you use a lucky egg for ultra friendships?"
+}
+
+func (p *PVP) EggForUltra(pieces []string, m *discordgo.MessageCreate, s *discordgo.Session) string {
+	player, ok := p.registering[m.Author.ID]
+	if !ok {
+		return "Well this is awkward. You need to start the registration process over. Sorry!"
+	}
+	response := p.parseAnswer(pieces)
+	if response == AnswerUnknown {
+		expectPM(m.ChannelID, p.EggForUltra)
+		return fmt.Sprintf("Sorry, I don't understand the answer '%s'. Please say 'yes' or 'no'.", strings.Join(pieces, " "))
+	}
+	if response == AnswerCancel {
+		delete(p.registering, m.Author.ID)
+		return "Okay, start again when you're ready"
+	}
+	if response == AnswerYes {
+		player.EggUltra = true
+	}
+	if response == AnswerNo {
+		player.EggUltra = false
+	}
+
 	expectPM(m.ChannelID, p.ConfirmInfo)
-	return fmt.Sprintf("Does this look right?\n\nIn-game name: %s\nFriend code: %s", player.IGN, player.FriendCode)
+	return fmt.Sprintf("Does this look right?\n\nIn-game name: %s\nFriend code: %s\nEgg for ultra: %v", player.IGN, player.FriendCode, player.EggUltra)
 }
 
 func (p *PVP) ConfirmInfo(pieces []string, m *discordgo.MessageCreate, s *discordgo.Session) string {
 	if _, ok := p.registering[m.Author.ID]; !ok {
 		return "Well this is awkward. You need to start the registration process over. Sorry!"
 	}
-	response := strings.ToLower(pieces[0])
-	if response == "y" || response == "yes" || response == "yep" || response == "yup" || response == "hell yeah" {
+	response := p.parseAnswer(pieces)
+	if response == AnswerYes {
 		return p.RegisterPlayer(m.Author.ID, s)
 	}
-	if response == "n" || response == "no" || response == "nope" || response == "nah" || response == "fuck you" {
+	if response == AnswerNo {
 		delete(p.registering, m.Author.ID)
 		expectPM(m.ChannelID, p.AskForFriendCode)
 		return "Okay, let's start over. What's your in-game name?"
 	}
-	if response == "c" || response == "cancel" || response == "stop" || response == "unsubscribe" {
+	if response == AnswerCancel {
 		delete(p.registering, m.Author.ID)
 		return "Okay, start again when you're ready"
 	}
 
 	expectPM(m.ChannelID, p.ConfirmInfo)
-	return fmt.Sprintf("Sorry, I don't understand the answer '%s'. Please say 'yes' or 'no'.", response)
+	return fmt.Sprintf("Sorry, I don't understand the answer '%s'. Please say 'yes' or 'no'.", strings.Join(pieces, " "))
 }
 
 func (p *PVP) RegisterPlayer(ID string, s *discordgo.Session) string {
@@ -299,4 +333,19 @@ func (p *PVP) RegisterPlayer(ID string, s *discordgo.Session) string {
 		}
 	}
 	return message
+}
+
+func (p *PVP) parseAnswer(pieces []string) Answer {
+	response := strings.ToLower(strings.Join(pieces, " "))
+	if response == "y" || response == "yes" || response == "yep" || response == "yup" || response == "hell yeah" {
+		return AnswerYes
+	}
+	if response == "n" || response == "no" || response == "nope" || response == "nah" || response == "fuck you" {
+		return AnswerNo
+	}
+	if response == "c" || response == "cancel" || response == "stop" || response == "unsubscribe" {
+		return AnswerCancel
+	}
+
+	return AnswerUnknown
 }
